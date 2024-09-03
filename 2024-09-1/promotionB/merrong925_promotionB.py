ec26a4727dd997bc4bbbcc55c55ff5d1277db999
Mingguriguri <rlaalswjd-@naver.com>
2024-09-03 13:10:40 +0900

    merrong925 / promotionB(BOJ #2435. 기상청 인턴 신현수) 주석 수정

--- 2024-09-1/promotionB/merrong925_promotionB.py
+++ 2024-09-1/promotionB/merrong925_promotionB.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 N, K = map(int, input().split())        # N: 온도 측정 전체 날짜. K: 합을 구하기 위한 연속적인 날짜 수
 temps = list(map(int, input().split())) # 온도 정수 수열
 
-prefix_sum = [0] * (N - K + 1) # 누적합 배열 초기화. 누적합 배열은 N에 K+1을 뺀 값만 필요함
+prefix_sum = [0] * (N - K + 1) # 누적합 배열 초기화. 누적합 배열은 (N-K)+1을 뺀 값만 필요함
 
 # 누적합 배열에 값 채워넣기
 for i in range(len(prefix_sum)):
